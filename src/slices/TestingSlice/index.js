e37787883859d41a0d7d508a49117645d1fb0cee
Lara Salameh <hello@larasalameh.com>
2024-06-03 21:08:12 +1000

    setting richText on TestingSlice for one field only

--- src/slices/TestingSlice/index.js
+++ src/slices/TestingSlice/index.js
@@ -1,3 +1,6 @@
+import { isFilled } from "@prismicio/client";
+import { PrismicNextLink, PrismicNextImage } from "@prismicio/next";
+import { PrismicRichText } from "@prismicio/react";
 /**
  * @typedef {import("@prismicio/client").Content.TestingSliceSlice} TestingSliceSlice
  * @typedef {import("@prismicio/react").SliceComponentProps<TestingSliceSlice>} TestingSliceProps
@@ -8,9 +11,15 @@ const TestingSlice = ({ slice }) => {
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className="new-section-test-by-lara"
     >
       Placeholder component for testing_slice (variation: {slice.variation})
       Slices
+      {isFilled.richText(slice.primary.Heading) && (
+        <div className="es-fullpage-hero__content__intro__headline">
+          <PrismicRichText field={slice.primary.Heading} />
+        </div>
+      )}
     </section>
   );
 };
